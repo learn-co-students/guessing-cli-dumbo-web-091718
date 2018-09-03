b890348e3aa26a040bd2d9bce73a3a9ed420bfbd
Nasser Sanou <nassersanou23@gmail.com>
2018-09-03 06:00:25 +0000

    guessing: done

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,20 @@
-# Code your solution here!
+def run_guessing_game
+  input = nil
+  computer_generated = nil
+
+
+  while input != "exit"
+    puts "Guess a number between 1 and 6."
+     input = gets.chomp
+   break if  input == "exit" || input.empty? # or input == nil?
+    computer_generated = rand(1..10)
+    if input.to_i == computer_generated
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{computer_generated}."
+    end
+  end
+
+  puts "Goodbye!"
+end
+run_guessing_game
