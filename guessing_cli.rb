3d2f458d18b913788b08db692dcccc53d08816b8
Nasser Sanou <nassersanou23@gmail.com>
2018-09-03 17:27:10 +0000

    added solution

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,6 @@ def run_guessing_game
   computer_generated = nil
   ask_user = "Guess a number between 1 and 4."
 
-
   while input != "exit"
     puts ask_user
     input = gets.chomp
@@ -22,6 +21,7 @@ def run_guessing_game
 end
 run_guessing_game
 
+#__________________________
 #BASIC testnumber = rand(3)
  
 # print "Type in your guess: "
@@ -38,3 +38,24 @@ run_guessing_game
 #     puts "Your guess is bigger that the hidden number"
 # end
 # end 
+#__________________________
+#SOLUTION
+# Code your solution here!
+def run_guessing_game
+  input = 0
+  random = nil
+
+  while input != "exit"
+    puts "Guess a number between 1 and 6."
+    input = gets.chomp
+    input == "exit" ? break : nil
+    random = rand(1..6)
+    if input.to_i == random
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{random}."
+    end
+  end
+
+  puts "Goodbye!"
+end
